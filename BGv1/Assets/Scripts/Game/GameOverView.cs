61887f6c63a7474af5473d539ef95c381100859e
norrisabu <norris@klab.com>
2014-01-10 14:01:50 +0800

    points and coins.

--- BGv1/Assets/Scripts/Game/GameOverView.cs
+++ BGv1/Assets/Scripts/Game/GameOverView.cs
@@ -16,11 +16,21 @@ public class GameOverView : BasicView {
 	private const string BTN_SHOP 	= "Anchor/BtnShop";
 	private const string BTN_MAIN 	= "Anchor/BtnMain";
 
+	private const string LBL_COINS = "Anchor/lblCoins";
+	private const string LBL_POINTS = "Anchor/lblPoints";
+
+	protected UILabel lblCoin;
+	protected UILabel lblPoints;
+	                              
+
 	void Awake() {
 		UIEventListener.Get(GetChild(BTN_RESUME)).onClick += OnClickResume;
 		UIEventListener.Get(GetChild(BTN_SHOP)).onClick += OnClickShop;
 		UIEventListener.Get(GetChild(BTN_MAIN)).onClick += OnClickMain;
 
+
+		lblCoin = GetChild (LBL_COINS).GetComponent<UILabel> ();
+		lblPoints = GetChild (LBL_POINTS).GetComponent<UILabel> ();
 	}
 
 	void OnClickResume(GameObject go) {
@@ -36,5 +46,13 @@ public class GameOverView : BasicView {
 	void OnClickMain(GameObject go) {
 		if(onClickMain != null)
 			onClickMain(go);
+	}
+
+	public void setCoins(int coins){
+		lblCoin.text = coins.ToString ();
+	}
+
+	public void setPoints(int points){
+		lblPoints.text = points.ToString ();
 	}
 }
